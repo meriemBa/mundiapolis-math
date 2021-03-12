2af7153bba02467129223575e39aec2929cc4dcd
meriemBa <76498829+meriemBa@users.noreply.github.com>
2021-03-12 11:27:32 +0100

    Create 4-line_up.py

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,7 @@
+def add_arrays(arr1, arr2):
+    if len(arr1) == len(arr2):
+        arr3 = []
+        for i in range(len(arr1)):
+            result.append(arr1[i] + arr2[i])
+        return arr3
+    return None
